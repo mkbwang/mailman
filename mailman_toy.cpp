caba456df1dc113efb2de2cf586fa537d3524486
wangmk <wangmk@umich.edu>
2019-10-07 20:38:51 -0400

    Adding more comments.

--- mailman_toy.cpp
+++ mailman_toy.cpp
@@ -11,34 +11,24 @@ using namespace std;
 
 // [[Rcpp::export]]
 colvec mailman_multiply(colvec& myvec, int rownum, int colnum){
-    // do the matrix multiplication from bottom up
-    // resize the vector sum up columns/rows
-    // dot product with corresponding "building blocks"
+    
     colvec result = arma::zeros<colvec>(rownum); 
     for (int i=0; i<rownum; i++){
       mat newmat(myvec);
       int matrow = colnum/pow(3,i);
-      newmat.set_size(matrow, pow(3,i));
+      newmat.set_size(matrow, pow(3,i));// reshape the matrix
       colvec colsum= arma::sum(newmat, 1);
       result(i) = arma::sum(colsum.subvec(int(matrow/3), int(matrow*2/3 - 1))) +
         arma::sum(colsum.tail(matrow/3)) * 2;
     }
     return result;
-    //mat newmat = myvec;
-    //newmat.set_size(3,9);
-    //cout << newmat << endl;
-    // cout << myvec << endl;
-    //cout << myvec.subvec(0,5) << endl;
+    
 }
 
 
 /*** R
 library(RcppArmadillo)
-# binarymat=matrix(0,3,8)
-# for (i in 1:3){
-#   element = rep(seq(0,1), each=8/(2^i))
-#   binarymat[i,]=rep(element, 2^(i-1))
-# }
+
 toy_mailman = function(rownum){
   # generate the matrix that contains only 0, 1 and 2
   colnum = 3^rownum
@@ -62,7 +52,6 @@ toy_mailman = function(rownum){
   print(paste(c("R time: ", Rtime[[1]]), collapse = ""))
   print(paste(c("Mailman time: ", mailmantime[[1]]), collapse = ""))
 }
-
 
 toy_mailman(14)
 
